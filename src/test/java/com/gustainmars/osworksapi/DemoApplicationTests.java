0bceed00f25dc542bc179948b39dd4a3acf0d108
gustasDesktop <gustaDesktop@github.com>
2020-06-21 23:28:53 -0300

    First commit, added simple class Controller

---
+++ src/test/java/com/gustainmars/osworksapi/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustainmars.osworksapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
